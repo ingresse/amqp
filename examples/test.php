9c57a9f642abfddb73361d89d45ecbdd64b01e0d
Taffarel de Lima <taffarel.lima@ingresse.com>
2016-03-22 11:55:19 -0300

    removing sensible data

--- examples/test.php
+++ examples/test.php
@@ -7,21 +7,14 @@ echo "------ Creating Adapter & Configs --------\n";
 
 $configData = [
     'connection' => [
-        'host' => '107.23.60.208',
+        'host' => 'localhost',
         'port' => 5672,
         'user' => 'guest',
         'pass' => 'guest',
         'vhost' => '/'
     ],
     'queues' => [
-        'supertramp' => [
-            'passive' => false,
-            'durable' => true,
-            'exclusive' => false,
-            'autoDelete' => false,
-            'delivery_mode' => 2
-        ],
-        'antifraud' => [
+        'worker.test' => [
             'passive' => false,
             'durable' => true,
             'exclusive' => false,
@@ -36,33 +29,21 @@ $configData = [
             'exclusive' => false,
             'noWait' => false
         ],
-        'Complexr' => [
-            'noLocal' => false,
-            'noAck' => false,
-            'exclusive' => false,
-            'noWait' => false
-        ]
+    ],
+    'logger' => [
+        'host' => 'localhost',
+        'port' => 6379,
+        'key' => 'logstash',
+        'channel' => 'message-queue-php'
     ]
 ];
 
 $config = new Ingresse\MessageQueuePHP\Config\AMQPConfig($configData);
 $adapter = new Ingresse\MessageQueuePHP\Adapter\AMQPAdapter($config);
 
-
 echo "------ Testing Publisher --------\n";
 
-$simplerProducer = new Ingresse\MessageQueuePHP\Publisher\Publisher($adapter, 'antifraud');
-
-for ($i = 1; $i <= 10000; $i++){
-    $data = ['id' => 467709,
-             'email' => 'my@email.com',
-             'domain' => 'email.com',
-             'name' => 'Teste'
-             ];
-    $simplerProducer
-        ->setMessage(json_encode($data))
-        ->send();
-}
+$testProducer = new Ingresse\MessageQueuePHP\Publisher\Publisher($adapter, 'worker.test');
 
 
 echo "------- Testing Subscriber -------\n";
@@ -70,6 +51,6 @@ echo "------- Testing Subscriber -------\n";
 $subscriber = new Ingresse\MessageQueuePHP\Subscriber\Subscriber($adapter);
 $simplerConsumer = new Ingresse\MessageQueuePHP\Subscriber\Consumer\SimplerConsumer;
 $subscriber
-    ->setConsumer($simplerConsumer)
-    ->subscribe('antifraud')
-    ->consume();
+   ->setConsumer($simplerConsumer)
+   ->subscribe('worker.test')
+   ->consume();
