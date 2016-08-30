d1eb4288d4c98c3735ea711087441711a1a3b656
Fernando Dias <fdias.webdev@gmail.com>
2016-08-30 17:28:34 -0300

    Change adapter factory method to create (#24)
    Change method name from construct to create to be called external
    
    Adjust for AMQP Config for right namespace

--- src/Adapter/FactoryAdapter.php
+++ src/Adapter/FactoryAdapter.php
@@ -2,12 +2,12 @@
 
 namespace MessageQueuePHP\Adapter;
 
-use MessageQueuePHP\Config\Config;
+use MessageQueuePHP\Config\AMQPConfig as Config;
 use MessageQueuePHP\Adapter\AMQPAdapter;
 
 class FactoryAdapter
 {
-    public function __construct(array $connection, array $logger)
+    public function create(array $connection, array $logger)
     {
         $params = ['connection' => $connection, 'logger' => $logger];
         return new AMQPAdapter(new Config($params));
